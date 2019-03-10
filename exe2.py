09b9ef0e899d413f9724754563895b230ea0c9a6
Shir Kehila <shirkpro@gmail.com>
2019-03-10 03:33:48 +0200

    Modify docstring return, add try except

--- exe2.py
+++ exe2.py
@@ -1,27 +1,33 @@
 def sum1():
     """Sum a list of numbers input 1 by 1
 
-    :return:
+    :return: the sum
     """
     _sum = 0
     num = input("Enter next number, to finish enter 'stop'")
-    while num != "stop":
-        _sum += int(num)
-        num = input("Enter next number, to finish enter 'stop'")
+    try:
+        while num != "stop":
+            _sum += int(num)
+            num = input("Enter next number, to finish enter 'stop'")
+    except ValueError:
+        print("You entered something different than a number")
     return _sum
 
 
 def sum2():
     """Sum a list of numbers in one input
 
-    :return:
+    :return: the sum
     """
     nums = input("Enter a sequence of numbers separated by commas:")
     # remove whitespaces
     nums = "".join(nums.split(" "))
     nums = nums.split(",")
-    nums = [int(num) for num in nums]
-    return sum(nums)
+    if all(x.isdigit() for x in nums):
+        nums = [int(num) for num in nums]
+        return sum(nums)
+    print("You entered something different than a number or wrong separator")
+    return None
 
 
 if __name__ == "__main__":
