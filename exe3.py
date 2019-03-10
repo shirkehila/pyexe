2979e4c95b3f129b33499fdcccecbd246c487f92
Shir Kehila <shirkpro@gmail.com>
2019-03-10 03:42:59 +0200

    Add a function to check main diagonal

--- exe3.py
+++ exe3.py
@@ -36,3 +36,23 @@ def check_col(board, col_num):
     if board[0][col_num] == board[1][col_num] == board[2][col_num]:
         return board[0][col_num]
     return 0
+
+
+def check_main_diagonal(board):
+    """Checks the main diagonal for winner
+
+    Args:
+        board: A 3*3 matrix with cells:
+            0 - empty
+            1 - player 1
+            2 - player 2
+
+    Returns:
+        0 - no winner
+        1 - player 1 won
+        2 - player 2 won
+    """
+
+    if board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+    return 0
