373d97f78bff2ef522d85e176499d898b0faff9d
Shir Kehila <shirkpro@gmail.com>
2019-03-10 05:12:48 +0200

    Add docstring

--- exe4.py
+++ exe4.py
@@ -2,6 +2,11 @@ from itertools import groupby
 
 
 def compress(str):
+    """Compresses a string by letter,count,letter,count...
+
+    :param str: a string to compress
+    :return: compressed string
+    """
     letters = list(str)
     compressed = ["{}{}".format(cnt, sum(1 for _ in group))
                   for cnt, group in groupby(letters)]
