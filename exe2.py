92458c61198a3ad1671b56f320ebdd0554762ffe
Shir Kehila <shirkpro@gmail.com>
2019-03-10 03:04:23 +0200

    Add docstrings and modify main

--- exe2.py
+++ exe2.py
@@ -1,11 +1,19 @@
 def sum1():
+    """Sum a list of numbers input 1 by 1
+
+    :return:
+    """
     pass
 
 
 def sum2():
+    """Sum a list of numbers in one input
+
+    :return:
+    """
     pass
 
 
 if __name__ == "__main__":
-    sum1()
-    sum1()
+    print("first sum is: {}".format(sum1()))
+    print("second sum is: {}".format(sum2()))
