2919fe1b3d165027c9f3aad3b241c849020a95e0
Shir Kehila <shirkpro@gmail.com>
2019-03-11 01:24:08 +0200

    Make a general decorator

--- exe7.py
+++ exe7.py
@@ -1,6 +1,19 @@
 import time
+lookup = {}
 
 
+def cache_decorator(function):
+    def wrapper(*args):
+        if (function.__name__, args) in lookup:
+            return lookup[(function.__name__, args)]
+        else:
+            result = function(*args)
+            lookup[(function.__name__, args)] = result
+            return result
+    return wrapper
+
+
+@cache_decorator
 def catalan(n):
     """A function to calculate catalan number recursively
     Catalan number are used in many combinatorial problems
@@ -13,7 +26,15 @@ def catalan(n):
     return sum(catalan(i)*catalan(n-1-i) for i in range(n))
 
 
+@cache_decorator
+def fib(n):
+    if n == 0 or n == 1:
+        return 1
+    return fib(n-1)+fib(n-2)
+
+
 if __name__ == '__main__':
     start_time = time.time()
-    print(catalan(15))
+    print(catalan(4))
+    print(fib(100))
     print("{} seconds".format(time.time()-start_time))
